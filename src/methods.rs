db5a8a3cc1ad41083c5d0e93307bcb738e2eceda
0xb-s <145866191+0xb-s@users.noreply.github.com>
2023-09-25 15:00:46 -0700

    Update methods.rs

--- src/methods.rs
+++ src/methods.rs
@@ -1,1 +1,49 @@
 
+use std::error::Error;
+use std::fmt;
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+enum MethodHttp {
+    GET,
+    POST,
+    //add more later
+}
+
+#[derive(Debug)]
+pub struct InvalidMethod;
+
+impl fmt::Display for InvalidMethod {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid HTTP Method")
+    }
+}
+
+impl Error for InvalidMethod {}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Method(MethodHttp);
+
+impl Method {
+    pub const GET: Self = Method(MethodHttp::GET);
+    pub const POST: Self = Method(MethodHttp::POST);
+
+    pub fn from_bytes(src: &[u8]) -> Result<Method, InvalidMethod> {
+        match src {
+            b"GET" => Ok(Method(MethodHttp::GET)),
+            b"POST" => Ok(Method(MethodHttp::POST)),
+            _ => Err(InvalidMethod),
+        }
+    }
+
+    pub fn is_read_only(&self) -> bool {
+        match self.0 {
+            MethodHttp::GET | MethodHttp::POST => true,
+          // add more later
+            _ => false,
+        }
+    }
+
+    pub fn is_same_result(&self) -> bool {
+        todo!()
+    }
+}
